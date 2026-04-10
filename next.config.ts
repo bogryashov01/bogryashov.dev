471c27ee8b059562a4cda8bb8b6302a4667156b0
Volodymyr Bogryashov <bogryashov01@gmail.com>
2026-04-10 20:49:11 +0300

    fix routes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,40 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   // Включаем поддержку типизированных роутов для лучшей работы с динамическими роутами
+  async redirects() {
+    return [
+      {
+        source: '/en',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/en/work',
+        destination: '/work',
+        permanent: true,
+      },
+      {
+        source: '/en/contact',
+        destination: '/contact',
+        permanent: true,
+      },
+      {
+        source: '/uk',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/uk/work',
+        destination: '/work',
+        permanent: true,
+      },
+      {
+        source: '/uk/contact',
+        destination: '/contact',
+        permanent: true,
+      },
+    ];
+  },
   typedRoutes: true,
 };
 
